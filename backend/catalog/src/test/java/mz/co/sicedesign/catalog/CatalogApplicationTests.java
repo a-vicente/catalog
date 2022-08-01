ab84d2648ad553f60faa0075a195e57b07e45d09
Arnaldo Vicente <arnaldorjvicente@gmail.com>
2022-08-01 14:08:47 +0200

    create backend project

---
+++ backend/catalog/src/test/java/mz/co/sicedesign/catalog/CatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package mz.co.sicedesign.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
